9335dfb04e24c2d1b6edb2cac8978379aae43822
Ovirne <balantaovirne@gmail.com>
2025-09-18 10:04:46 -0500

    Agregado el componente de login

---
+++ src/app/login/main-login/main-login.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-main-login',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+ templateUrl: './main-login.component.html',
+  styleUrls:['./main-login.component.css']
+})
+
+export class MainLoginComponent{
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private auth: AuthService, private router: Router) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.invalid) return;
+
+    this.auth.login(this.loginForm.value).subscribe({
+      next: (res: any) => {
+        localStorage.setItem('token', res.token); // Guardar token
+        alert('Login exitoso');
+        this.router.navigate(['/register']); // Ajusta ruta destino
+      },
+      error: () => alert('Usuario o contraseña incorrectos')
+    });
+  }
+}
+
+
---
+++ src/app/login/nabvar/nabvar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nabvar',
+  imports: [],
+  templateUrl: './nabvar.component.html',
+  styleUrl: './nabvar.component.css'
+})
+export class NabvarComponent {
+
+}
